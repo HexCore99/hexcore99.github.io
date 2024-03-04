bef33c5ab3214c4ead0ebb48800ad128a8ff6773
HexCore99 <aust1438@gmail.com>
2024-03-04 12:38:11 +0600

    initial commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const preTags = document.getElementsByTagName('pre');
 
-for (let i = 0; i < preTags.length; i++) {
-  let text = preTags[i].innerHTML;
+
+//const preTags = document.getElementsByTagName('pre');
+const answerElements = document.getElementsByClassName('answer');
+
+// for (let i = 0; i < preTags.length; i++) {
+for (let i = 0; i < answerElements.length; i++) {
+  let text = answerElements[i].innerHTML;
 
   text = text.replace(/\b\d+\b/g, '<span class="integer">$&</span>'); // Match integers
   text = text.replace(/\%[a-z]/g, '<span class="format-specifier">$&</span>'); // Match format specifiers 
   text = text.replace(/\(/g, '<span class="bracket">(</span>')
   .replace(/\)/g, '<span class="bracket">)</span>');
-
 
 
   text = text.replace(/\(/g, '<span class="bracket">(</span>')
@@ -21,15 +24,6 @@ text = text.replace(/\[/g, '<span class="Third-bracket">[</span>')
 text = text.replace(/\{/g, '<span class="curly-braces">{</span>')
   .replace(/\}/g, '<span class="curly-braces">}</span>');// For-Curly Braces {}
 
-  //text = text.replace(/#include\s*<stdio.h>\s*/g, '...'); // for adding space after < and before >
-
-
-//   text = text.replace(/#include\s*(<[^>]+>)/g, '#include &lt;$1&gt;'); 
-text = text.replace(/#include\s*<([^\s>]+)>/g, '#include < $1 >');
-
-// 2. Color the text within the < > brackets (modified)
-text = text.replace(/#include\s*<(.*?)>/g, '<span class="preprocessor">#include</span> <span class="header"><$1></span>'); 
-
   // Color data types (you can adjust the colors later)
   text = text.replace(/\bint\b/g, '<span class="int-type">int</span>'); 
   text = text.replace(/\bfloat\b/g, '<span class="float-type">float</span>'); 
@@ -39,6 +33,6 @@ text = text.replace(/#include\s*<(.*?)>/g, '<span class="preprocessor">#include<
 
   const modifiedText = text.replace(/\bfor\b/g, '<span class="highlight">for</span>');
   
-  preTags[i].innerHTML = modifiedText;
+  answerElements[i].innerHTML = modifiedText;
 
 }
