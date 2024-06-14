67f702302339a6108f9b474a4c4d5b9933009f59
HexCore99 <aust1438@gmail.com>
2024-06-14 11:28:42 +0600

    Added OOP Random Question Solve

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -11,30 +11,39 @@ export default defineConfig({
   //     { text: '20', link: '/20' }
   //   ]
   // },
-
+title: 'Siabul Hassan Siyam',
+description:  "Solve Bank",
   themeConfig: {
-    sidebar: {
-      '/': [
-        {
-          text: 'SPL',
-          items: [
+    sidebar: [
+
+      {
+        text: 'SPL SOLVE',
+        items: [
+          {
+            text: 'PROBLEM SET',
+            collapsed: true,
+            items: [
+              { text: 'Loop', link: '/loops/singleLoop' },
+              { text: 'Nested Loop', link: '/nestedLoop/nestedLoop' },
+              { text: '1D Array', link: '/1DArray/1DArray' },
+              { text: '2D Array', link: '/2DArray/2DArray' },
+              { text: 'Strings', link: '/Strings/Strings' },
+              { text: 'Function', link: '/Functions/functions' },
+              { text: 'Pointers', link: '/Pointers/pointers' },
+              { text: 'Structure', link: '/Structures/structures' },
+              { text: 'Recursion', link: '/Recursion/recursion' },
+            ]
+          }, // 1st ITEM   END
+          
+          //If ANy item need to add. inside **********SPL SOLVE********** ADD HERE
+          {
+
+          },
+            
+          
+            //2nd ITEM
             {
-              text: 'Problems',
-              collapsed: true,
-              items: [
-                { text: 'Loop', link: '/loops/singleLoop' },
-                { text: 'Nested Loop', link: '/nestedLoop/nestedLoop' },
-                { text: '1D Array', link: '/1DArray/1DArray' },
-                { text: '2D Array', link: '/2DArray/2DArray' },
-                { text: 'Strings', link: '/Strings/Strings' },
-                { text: 'Function', link: '/Functions/functions' },
-                { text: 'Pointers', link: '/Pointers/pointers' },
-                { text: 'Structure', link: '/Structures/structures' },
-                { text: 'Recursion', link: '/Recursion/recursion' },
-              ],
-            },
-            {
-              text: 'Mid Solve',
+              text: 'MID SOLVE',
               collapsed: true,
               items: [
                 { text: 'Spring 22', link: '/MidSolve/spring_22/spring_22' },
@@ -44,31 +53,78 @@ export default defineConfig({
                 { text: 'Spring 23', link: '/MidSolve/spring_23/spring_23' },
                 { text: 'Spring 24', link: '/MidSolve/spring_24/spring_24' },
               ]
-            },
+            }, //5th Item End
+
+             //3rd ITEM
             {
-              text: 'Final Solve',
+              text: 'FINAL SOLVE',
               collapsed: true,
               items: [
                 { text: 'Fall 23', link: '/FinalSolve/Fall23/fall23F' },
                 { text: 'Spring 23', link: '/FinalSolve/Summer23/summer23F' },
-                { text: 'Spring 24', link: '/FinalSolve/Spring24/sprng24F' },
+                { text: 'Spring 24', link: '/FinalSolve/Spring24/spring24F' },
               ]
-            }
-          ],
-        }
-      ],
-      '/MyProjects/': [
-        {
-          text: 'My Project',
-          items: [
-            { text: 'Introduction', link: '/my-project/introduction' },
-            { text: 'Features', link: '/my-project/features' },
-            { text: 'Setup', link: '/my-project/setup' },
-            { text: 'Usage', link: '/my-project/usage' },
-          ]
-        }
+            }, //3rd  Item End
+
+    // if anything need  to add under the SPL SOLVE section, add here, 
+            //4th ITEM
+            {
+              
+
+
+
+            },// 4th ITEM END
+
+        ] // Root Items Items END
+      },  // ROOT ITEM END
+
+      // *************************************** New SECTION FOR  OOP *************************************************
+
+{
+  text: 'OOP',
+  
+  items: [
+    
+    {
+      text: "Random Question Solve",
+      collapsed: true,
+      items:[
+        {text: "Random", link: '/OOP/randomProblem'},
       ]
+    },
+    
+
+    // MID SECTION
+    {
+
+      text: "MID SOLVE",
+      collapsed: true,
+      items:[
+
+      ]
+    }, // MID SECTION END
+
+    // FINAL SECTION
+    {
+      text: "FINAL SOLVE",
+      collapsed: true,
+      items:[
+
+      ]
+    }, // FINAL SECTION END
+    
+    {
+      // Append other section with **********************OOP****************
     }
+    
+  ]
+}// OOP SECTION END --------------------
+
+
+
+
+
+    ] // Side Bar END 
   },
 
   markdown: {
