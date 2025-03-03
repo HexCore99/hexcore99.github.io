89d8d51603afdf8c8f89b6219611b8f3317323aa
HexCore99 <shassan2330322@bscse.uiu.ac.bd>
2025-03-03 10:45:35 +0600

    Update stylesheet link in VitePress config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -163,8 +163,8 @@ items:[
   },
 
   head: [
-    ['link', { rel: 'stylesheet', href: '/style.css',
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
+    ['link', { rel: 'stylesheet', href: 'style.css',
+      // href: 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
       integrity: 'sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV',
       crossorigin: 'anonymous'
      }]
